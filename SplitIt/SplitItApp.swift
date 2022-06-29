c22eb83223f0134d59c8d76fab408fd369203a34
Rohit Patidar <patidar_rohit@yahoo.co.in>
2022-06-29 12:43:21 -0400

    Initial Commit

---
+++ SplitIt/SplitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItApp.swift
+//  SplitIt
+//
+//  Created by Rohit Patidar on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
